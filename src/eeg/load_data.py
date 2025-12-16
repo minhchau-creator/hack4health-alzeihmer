c0ec5733733a9466ecdd561794508646df1429a0
minhchau-creator <84848387+minhchau-creator@users.noreply.github.com>
2025-12-16 17:14:46 +0700

    hello

--- src/eeg/load_data.py
+++ src/eeg/load_data.py
@@ -1,9 +1,46 @@
-# src/eeg/load.py
 import mne
+import pandas as pd
+from pathlib import Path
 
-def load_raw_eeg(file_path: str):
-    """
-    Load raw EEG file.
-    """
-    raw = mne.io.read_raw_edf(file_path, preload=True, verbose=False)
-    return raw
+def load_raw_eeg(data_root):
+    raws = []
+    labels = []
+    meta = []   # lưu open/close eye
+
+    data_root = Path(data_root)
+
+    participants = pd.read_csv(
+        data_root / "participants.tsv",
+        sep="\t"
+    )
+
+    for _, row in participants.iterrows():
+        pid = row["participant_id"]
+        label = row["group"]
+
+        eeg_dir = data_root / pid / "eeg"
+        if not eeg_dir.exists():
+            continue
+
+        # LẤY TẤT CẢ FILE EDF
+        edf_files = sorted(eeg_dir.glob("*.edf"))
+
+        for edf_path in edf_files:
+            raw = mne.io.read_raw_edf(
+                edf_path,
+                preload=True,
+                verbose=False
+            )
+
+            raws.append(raw)
+            labels.append(label)
+
+            # metadata: open / close eye
+            if "open" in edf_path.name.lower():
+                meta.append("open")
+            elif "close" in edf_path.name.lower():
+                meta.append("close")
+            else:
+                meta.append("unknown")
+
+    return raws, labels, meta
